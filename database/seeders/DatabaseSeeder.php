498117fcc5f52b57b37ee7418bad52635cca279a
Cariver30 <cariver30@gmail.com>
2025-12-29 12:48:20 -0400

    Add WineSeeder to load cava dataset

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             SettingSeeder::class,
             AdminUserSeeder::class,
             MenuCafeSeeder::class,
+            WineSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/WineSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class WineSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sqlPath = database_path('cava_seed.sql');
+
+        if (! File::exists($sqlPath)) {
+            $this->command?->warn('El archivo cava_seed.sql no existe, se omitió el seed de vinos.');
+            return;
+        }
+
+        $sql = File::get($sqlPath);
+
+        // Ajustar instrucciones al dialecto MySQL.
+        $sql = str_replace(
+            ['PRAGMA foreign_keys=OFF;', '"'],
+            ['SET FOREIGN_KEY_CHECKS=0;', '`'],
+            $sql
+        );
+
+        $sql .= PHP_EOL . 'SET FOREIGN_KEY_CHECKS=1;';
+
+        DB::unprepared($sql);
+    }
+}
